687dfc77ba726f588d71d379b5533462a013eb63
SWE-Jaffar <Jaffar4k@yahoo.com>
2023-10-24 03:06:11 +0300

    todoapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState,useRef } from 'react';
 function App() {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef();
+  const handleClick = () => {
+      const text = inputRef.current.value;
+      const newItem = {done:false , text}
+      //spread operator
+      setTodos([...todos,newItem]);
+      //Make it empty after adding the task
+      inputRef.current.value = '';
+  };
+  const handleItemDone = (index) => {
+    //get copy of the array
+    const newTodos = [...todos];
+    //update the done property of the item
+    newTodos[index].done = !newTodos[index].done;
+    //update the state
+    setTodos(newTodos);
+  
+  };
+  const handelDelete = (index) => {
+    //get copy of the array
+    const newTodos = [...todos];
+    //remove the item from the array
+    newTodos.splice(index,1);
+    //update the state
+    setTodos(newTodos);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>To Do List</h2>
+      <div className='to-do'>
+      <ul>
+       {
+        todos.map(({text,done},index)=>{
+          return <div className='item'>
+            
+            <li className= {done ? "done": ""} key={index} onClick={ () => handleItemDone(index)}>{text}</li>
+          <span onClick={()=> handelDelete(index)}>❌</span>
+          
+          </div>
+        })
+
+
+       }
+      </ul>
+      <input ref={inputRef} placeholder='Enter Your Task.....'/>
+      <button onClick={handleClick}>Add</button>
+      
+      </div>
+      
     </div>
   );
 }
